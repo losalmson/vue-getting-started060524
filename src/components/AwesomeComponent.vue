3b68075857ca05b7589f9e15943e0705f170b814
losalmson <mialo.salmson@gmail.com>
2024-05-08 12:05:42 +0200

    Addded classes with v-bind

--- src/components/AwesomeComponent.vue
+++ src/components/AwesomeComponent.vue
@@ -5,7 +5,8 @@ import { ref } from "vue";
 defineProps<{ msg: string }>();
 
 const count: number = ref(0);
-
+const myBtnClass = "btn-awesome";
+const myDivClass = "card-awesome";
 const myName: string = "Lo";
 
 </script>
@@ -14,10 +15,10 @@ const myName: string = "Lo";
     <!-- interpolation of msg  -->
     <h1> {{ msg }}</h1>
 
-    <div class="card"></div>
+    <div v-bind:class="myDivClass"></div>
         <!-- interpolation -->
         <h3>Oh btw, my name is {{ myName }} and this is my first Vue project:3</h3>
         <p>press this button if u think components r awesome!!</p>
         <!-- v-on with @ to press btn and interpolation -->
-        <button type="button" @click="count++">awesome x {{ count }}</button>
+        <button type="button" :class="myBtnClass" @click="count++">awesome x {{ count }}</button>
 </template>
